e34f1397518aa3e9fb976718dec64c043e98bfb7
Victor Martins <sistemaprogressao@gmail.com>
2017-05-11 04:03:15 -0300

    Update urls.py. Found.

--- SistemaProgressao/urls.py
+++ SistemaProgressao/urls.py
@@ -3,19 +3,18 @@ from . import views
 
 # definindo a chamada da view, o arquivo HTML
 urlpatterns = [
-	url(r'^$', views.login),
+	url(r'^$', views.apresentacao),
 	url(r'^cadastrar/$', views.cadastrar),
 	url(r'^login/$', views.login),	
-	
-	url(r'^historico/$', views.historico),
 	url(r'^documents/$', views.documents),
+	url(r'^infogerais/$', views.infogerais),
+	url(r'^main/', views.main),
+	url(r'^historico/$', views.historico),
 	url(r'^anexoi/$', views.anexoi),
 	url(r'^anexoii/$', views.anexoii),
 	url(r'^anexoiii/$', views.anexoiii),
-	url(r'^anexoiv/$', views.anexoiv),
-
-	url(r'^infogerais/$', views.infogerais),
-	
-	url(r'^main/$', views.main),
+	url(r'^anexoiv/$', views.anexoiv),	
 	url(r'^apresentacao/$', views.apresentacao),
 ]
+
+# funcionandooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
